df798bb1e695544c00dd90fe6146238cc6af7c01
dyrs.zc <dyrs.zc@outlook.com>
2018-04-26 17:00:24 +0800

    cool guy

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 console.log('good cool')
 
 document.write('memeda')
+
+class coolguy {
+    constructor() {
+        this.name = 'coolboy'
+        this.age = 291
+        this.hobby = 'cool'
+    }
+}
+
+
+
+const me = new  coolguy()
+
+console.dir(me)
